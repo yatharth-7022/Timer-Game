9bac0ec022e1deecd94acd9058c4cab41c6d5e6e
yatharth-7022 <yatharth7022@gmail.com>
2024-10-06 23:13:43 +0530

    made sucess pop up and adeed game score and completed the app

--- src/components/resultModal.jsx
+++ src/components/resultModal.jsx
@@ -1,18 +1,25 @@
 import React, { forwardRef } from "react";
 
-export const ResultModal = forwardRef(({ targetTime, result }, ref) => {
-  return (
-    <dialog ref={ref} className="result-modal">
-      <h2>{result}</h2>
-      <p>
-        The target timer was <strong>{targetTime} seconds.</strong>
-      </p>
-      <p>
-        You stopped the timer with <strong>X seconds </strong>left
-      </p>
-      <form method="dialog">
-        <button>Close</button>
-      </form>
-    </dialog>
-  );
-});
+export const ResultModal = forwardRef(
+  ({ targetTime, remainingTime, onReset }, ref) => {
+    const userLost = remainingTime <= 0;
+    const formattedTime = (remainingTime / 1000).toFixed(2);
+    const result = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+    return (
+      <dialog ref={ref} className="result-modal" onClose={onReset}>
+        {userLost && <h2>You Lost</h2>}
+        {!userLost && <h2>Your score: {result}</h2>}
+        <p>
+          The target timer was <strong>{targetTime} seconds.</strong>
+        </p>
+        <p>
+          You stopped the timer with <strong>{formattedTime} seconds </strong>
+          left
+        </p>
+        <form method="dialog" onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>
+    );
+  }
+);
--- src/components/timerChallenge.jsx
+++ src/components/timerChallenge.jsx
@@ -15,8 +15,11 @@ export const TimerChallenge = ({ title, targetTime }) => {
   }
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
+
     dialog.current.showModal();
+  }
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
   function handleStop() {
     dialog.current.showModal();
@@ -25,7 +28,12 @@ export const TimerChallenge = ({ title, targetTime }) => {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="You lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         {/* {timerExpired && <p>You lost !</p>} */}
